77444d1628bf9d0f8a0d10fd40694122687c8a19
Felhof <Felhof1@hotmail.com>
2021-08-29 13:33:27 +0100

    add some details for visualising evaluation

--- trainer.py
+++ trainer.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 from Discrete_SAC_Agent import SACAgent
 
 TRAINING_EVALUATION_RATIO = 10
-RUNS = 15
-EPISODES_PER_RUN = 500
+RUNS = 5
+EPISODES_PER_RUN = 401
+STEPS_PER_EPISODE = 200
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v1")
-    agent = SACAgent(env)
     agent_results = []
     for _ in range(RUNS):
+        agent = SACAgent(env)
         run_results = []
         for episode_number in range(EPISODES_PER_RUN):
             evaluation_episode = episode_number % TRAINING_EVALUATION_RATIO == 0
@@ -20,7 +21,7 @@ if __name__ == "__main__":
             state = env.reset()
             done = False
             i = 0
-            while not done and i < 200:
+            while not done and i < STEPS_PER_EPISODE:
                 i += 1
                 action = agent.get_next_action(state, evaluation_episode=evaluation_episode)
                 next_state, reward, done, info = env.step(action)
@@ -31,18 +32,26 @@ if __name__ == "__main__":
                 state = next_state
             if evaluation_episode:
                 run_results.append(episode_reward)
+        agent_results.append(run_results)
 
     env.close()
 
-    results_mean = [np.mean([agent_result[n] for agent_result in agent_results]) for n in range(EPISODES_PER_RUN)]
-    results_std = [np.std([agent_result[n] for agent_result in agent_results]) for n in range(EPISODES_PER_RUN)]
+    n_results = EPISODES_PER_RUN // TRAINING_EVALUATION_RATIO
+    results_mean = [np.mean([agent_result[n] for agent_result in agent_results]) for n in range(n_results)]
+    results_std = [np.std([agent_result[n] for agent_result in agent_results]) for n in range(n_results)]
     mean_plus_std = [m + s for m, s in zip(results_mean, results_std)]
     mean_minus_std = [m - s for m, s in zip(results_mean, results_std)]
 
     x_vals = list(range(len(results_mean)))
+    x_vals = [x_val * 10 for x_val in x_vals]
 
     ax = plt.gca()
+    ax.set_ylim([0, 200])
+    ax.set_ylabel('Episode Scores')
+    ax.set_xlabel('Episode Number')
     ax.plot(x_vals, results_mean, label='Discrete SAC', color='blue')
     ax.plot(x_vals, mean_plus_std, color='blue', alpha=0.1)
     ax.plot(x_vals, mean_minus_std, color='blue', alpha=0.1)
     ax.fill_between(x_vals, y1=mean_minus_std, y2=mean_plus_std, alpha=0.1, color='blue')
+    plt.legend(loc='best')
+    plt.show()
